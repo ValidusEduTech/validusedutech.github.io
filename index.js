6f3d91faf6294b41ee1a0775d98fefc72a7ed9d2
ValidusEduTech <137680257+ValidusEduTech@users.noreply.github.com>
2023-08-04 11:45:17 +0530

    files added to update the website
    deleted previous files and now added new files

---
+++ index.js
@@ -0,0 +1,102 @@
+$(".nav-items").hover(function(){
+    $(".nav-items").addClass("item-pink")
+}, function(){
+    $(".nav-items").removeClass("item-pink")
+})
+
+$(".homeButton").hover(function(){
+    $(".homeButton").addClass("homeButtonHover")
+}, function(){
+    $(".homeButton").removeClass("homeButtonHover")
+})
+
+$(".howButton").hover(function(){
+    $(".howButton").addClass("howButtonHover")
+}, function(){
+    $(".howButton").removeClass("howButtonHover")
+})
+
+$(".forthComingEventButton").hover(function(){
+    $(".forthComingEventButton").addClass("forthComingEventButtonHover")
+}, function(){
+    $(".forthComingEventButton").removeClass("forthComingEventButtonHover")
+})
+
+$(".joinButton").hover(function(){
+    $(".joinButton").addClass("joinButtonHover")
+}, function(){
+    $(".joinButton").removeClass("joinButtonHover")
+})
+
+
+// ________________________________________________________
+$(".aboutGoalCol1").hover(function(){
+    $(".aboutGoalCol1").addClass("aboutGoalColhover")
+}, function(){
+    $(".aboutGoalCol1").removeClass("aboutGoalColhover")
+})
+
+
+$(".aboutGoalCol2").hover(function(){
+    $(".aboutGoalCol2").addClass("aboutGoalColhover")
+}, function(){
+    $(".aboutGoalCol2").removeClass("aboutGoalColhover") 
+})
+
+$(".aboutGoalCol3").hover(function(){
+    $(".aboutGoalCol3").addClass("aboutGoalColhover")
+}, function(){
+    $(".aboutGoalCol3").removeClass("aboutGoalColhover") 
+})
+
+$(".aboutGoalCol4").hover(function(){
+    $(".aboutGoalCol4").addClass("aboutGoalColhover")
+}, function(){
+    $(".aboutGoalCol4").removeClass("aboutGoalColhover") 
+})
+
+$(".aboutGoalCol5").hover(function(){
+    $(".aboutGoalCol5").addClass("aboutGoalColhover")
+}, function(){
+    $(".aboutGoalCol5").removeClass("aboutGoalColhover") 
+})
+
+$(".aboutGoalCol6").hover(function(){
+    $(".aboutGoalCol6").addClass("aboutGoalColhover")
+}, function(){
+    $(".aboutGoalCol6").removeClass("aboutGoalColhover") 
+})
+
+//----------------------------------------------
+$(".eligibilityCard1").hover(function(){
+    $(".eligibilityCard1").addClass("eligibilityCardHover")
+}, function(){
+    $(".eligibilityCard1").removeClass("eligibilityCardHover") 
+})
+$(".eligibilityCard2").hover(function(){
+    $(".eligibilityCard2").addClass("eligibilityCardHover")
+}, function(){
+    $(".eligibilityCard2").removeClass("eligibilityCardHover") 
+})
+//------------------------------------------------------------
+$(".keyAdmissionCard1").hover(function(){
+    $(".keyAdmissionCard1").addClass("keyAdmissionCardHover")
+}, function(){
+    $(".keyAdmissionCard1").removeClass("keyAdmissionCardHover") 
+})
+$(".keyAdmissionCard2").hover(function(){
+    $(".keyAdmissionCard2").addClass("keyAdmissionCardHover")
+}, function(){
+    $(".keyAdmissionCard2").removeClass("keyAdmissionCardHover") 
+})
+$(".keyAdmissionCard3").hover(function(){
+    $(".keyAdmissionCard3").addClass("keyAdmissionCardHover")
+}, function(){
+    $(".keyAdmissionCard3").removeClass("keyAdmissionCardHover") 
+})
+
+
+
+   
+
+
